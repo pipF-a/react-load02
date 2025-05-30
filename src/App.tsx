80e7da98d1761560032b72a4619cd48a0b5729a3
pipF-a <pip87213@gmail.com>
2025-05-30 10:15:27 +0900

    リファクタ

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { TechQuotes } from "./components/TechQuotes";
 function App() {
   return (
     <>
-    <div className="bg-gray-100 min-h-screen pt-16 pb-8 space-y-8">
+    <div className="bg-gray-100 min-h-screen pt-16 pb-8 space-y-8 p-8">
       <h1 className="text-5xl font-bold text-center">Random Pokemon Generator</h1>
-      <p className="text-gray-700 text-center">ボタンを押すとランダムにポケモンが出現し亜mす</p>
+      <p className="text-gray-700 text-center">ボタンを押すとランダムにポケモンが出現します</p>
       <TechQuotes/>
       <footer>
         <p className="text-gray-700 text-center">Created by <a className="text-blue-600" href="#" target="_blank" rel="noreferrer">@your-handle</a> © 2025</p>
